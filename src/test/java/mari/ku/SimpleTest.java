1a4749ec14f35f0b3bf73e1f34250c778e957d0e
Marina Kucherenko <marina.ku4erenko@yandex.ru>
2022-02-05 14:05:16 +0300

    commit1

---
+++ src/test/java/mari/ku/SimpleTest.java
@@ -0,0 +1,14 @@
+package mari.ku;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+    void test0() {
+        assertTrue(false);
+
+    }
+}
